b339d394887f537cc66284564866d7c5be3d90fd
COCOA-PC\Cocoa <mtj2010@naver.com>
2021-09-18 01:52:22 +0900

    능력 추가 필요 각주 추가

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/gunner/AntimaterRifle.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/gunner/AntimaterRifle.java
@@ -2,3 +2,4 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.gunner;
 
 public class AntimaterRifle {
 }
+//TODO: 능력 추가 필요
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/gunner/Buckshot.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/gunner/Buckshot.java
@@ -2,3 +2,4 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.gunner;
 
 public class Buckshot {
 }
+//TODO: 능력 추가 필요
